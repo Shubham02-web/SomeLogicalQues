b16a9c8a2e660ee79845482302273a62837afff9
Shubham Vishwakarma <vi453770@gmail.com>
2025-02-22 18:45:46 +0530

    Reverese pattern DOone

--- logic.js
+++ logic.js
@@ -5,13 +5,31 @@
 // ****
 // *****
 
-function starPattern(n) {
-  for (let i = 1; i <= n; i++) {
-    let row = "";
-    for (let j = 0; j < i; j++) {
-      row = row + "*";
-    }
-    console.log(row);
-  }
-}
-console.log(starPattern(5));
+// function starPattern(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 0; j < i; j++) {
+//       row = row + "*";
+//     }
+//     console.log(row);
+//   }
+// }
+// console.log(starPattern(5));
+
+// Q2) reverse star pattern
+// *****
+// ****
+// ***
+// **
+// *
+
+// function reversePattern(n) {
+//   for (let i = n; i > 0; i--) {
+//     let temp = "";
+//     for (let j = 0; j < i; j++) {
+//       temp = temp + "*";
+//     }
+//     console.log(temp);
+//   }
+// }
+// reversePattern(5);
